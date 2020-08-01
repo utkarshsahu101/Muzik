3df59f7214cce16ffaf7d7772bf0bc71ffc56284
Utkarsh Sahu <utkarsh.sahu@alphafortress.com>
2020-08-01 14:19:51 +0530

    navbar is added

--- index.php
+++ index.php
@@ -19,75 +19,106 @@ else {
 </head>
 
 <body>
-    <div id="nowPlayingBarContainer">
-        <div id="nowPlayingBar">
-            <div id="nowPlayingLeft">
-                <div class="content">
-                    <span class="albumLink">
-                        <img src="assets/images/square.jpg" alt="" class="albumArtwork">
-                    </span>
-                    <div class="trackInfo">
-                        <span class="trackName">
-                            <span>Happy Birthday</span>
+    <div id="mainContainer">
+        <div id="topContainer">
+            <div id="navBarContainer">
+                <nav class="navBar">
+                    <a href="index.php" class="logo">
+                        <img src="assets/images/logo.png" alt="">
+                    </a>
+                    <div class="group">
+                        <div class="navItem">
+                            <a href="search.php" class="navItemLink">Search
+                                <img src="assets/images/icons/search.png" class="icon" alt="Search">
+                            </a>
+                        </div>
+                    </div>
+                    <div class="group">
+                        <div class="navItem">
+                            <a href="browse.php" class="navItemLink">Browse</a>
+                        </div>
+                        <div class="navItem">
+                            <a href="yourMusic.php" class="navItemLink">Your Music</a>
+                        </div>
+                        <div class="navItem">
+                            <a href="profile.php" class="navItemLink">Utkarsh Sahu</a>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </div>
+        <div id="nowPlayingBarContainer">
+            <div id="nowPlayingBar">
+                <div id="nowPlayingLeft">
+                    <div class="content">
+                        <span class="albumLink">
+                            <img src="assets/images/square.jpg" alt="" class="albumArtwork">
                         </span>
-                        <span class="artistName">
-                            <span>Utkarsh Sahu</span>
-                        </span>
+                        <div class="trackInfo">
+                            <span class="trackName">
+                                <span>Happy Birthday</span>
+                            </span>
+                            <span class="artistName">
+                                <span>Utkarsh Sahu</span>
+                            </span>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div id="nowPlayingCenter">
-                <div class="content playerControls">
-                    <div class="buttons">
+                <div id="nowPlayingCenter">
+                    <div class="content playerControls">
+                        <div class="buttons">
 
-                        <button class="controlButton shuffle" title="Shuffle button">
-                            <img src="assets/images/icons/shuffle.png" alt="Shuffle">
-                        </button>
+                            <button class="controlButton shuffle" title="Shuffle button">
+                                <img src="assets/images/icons/shuffle.png" alt="Shuffle">
+                            </button>
 
-                        <button class="controlButton previous" title="Previous button">
-                            <img src="assets/images/icons/previous.png" alt="Previous">
-                        </button>
+                            <button class="controlButton previous" title="Previous button">
+                                <img src="assets/images/icons/previous.png" alt="Previous">
+                            </button>
 
-                        <button class="controlButton play" title="Play button">
-                            <img src="assets/images/icons/play.png" alt="Play">
-                        </button>
+                            <button class="controlButton play" title="Play button">
+                                <img src="assets/images/icons/play.png" alt="Play">
+                            </button>
 
-                        <button class="controlButton pause" title="Pause button" style="display: none;">
-                            <img src="assets/images/icons/pause.png" alt="Pause">
-                        </button>
+                            <button class="controlButton pause" title="Pause button" style="display: none;">
+                                <img src="assets/images/icons/pause.png" alt="Pause">
+                            </button>
 
-                        <button class="controlButton next" title="Next button">
-                            <img src="assets/images/icons/next.png" alt="Next">
-                        </button>
-                        
-                        <button class="controlButton repeat" title="Repeat button">
-                            <img src="assets/images/icons/repeat.png" alt="Repeat">
-                        </button>
+                            <button class="controlButton next" title="Next button">
+                                <img src="assets/images/icons/next.png" alt="Next">
+                            </button>
+                            
+                            <button class="controlButton repeat" title="Repeat button">
+                                <img src="assets/images/icons/repeat.png" alt="Repeat">
+                            </button>
 
+                        </div>
+                        <div class="playbackBar">
+                            <span class="progressTime current">0.00</span>
+                            <span class="progressBar">
+                                <div class="progressBarBg">
+                                    <div class="progress"></div>
+                                </div>
+                            </span>
+                            <span class="progressTime remaining">0.00</span>
+                        </div>
                     </div>
-                    <div class="playbackBar">
-                        <span class="progressTime current">0.00</span>
+                </div>
+                <div id="nowPlayingRight">
+                    <div class="volumeBar">
+                        <button class="controlButton volume" title="Volume button">
+                            <img src="assets/images/icons/volume.png" alt="volume">
+                        </button>
                         <span class="progressBar">
                             <div class="progressBarBg">
                                 <div class="progress"></div>
                             </div>
                         </span>
-                        <span class="progressTime remaining">0.00</span>
                     </div>
                 </div>
             </div>
-            <div id="nowPlayingRight">
-                <div class="volumeBar">
-                    <button class="controlButton volume" title="Volume button">
-                        <img src="assets/images/icons/volume.png" alt="volume">
-                    </button>
-                    <span class="progressBar">
-                        <div class="progressBarBg">
-                            <div class="progress"></div>
-                        </div>
-                    </span>
-                </div>
-            </div>
+        
+        
         </div>
     </div>
 </body>
