7d7a293fc0b0783047a9ea8f49d14d8aa4346d43
Utkarsh Sahu <utkarsh.sahu@alphafortress.com>
2020-08-01 10:43:50 +0530

    dashboard is completed

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+include('includes/config.php');
+
+// session_destroy();
+
+if(isset($_SESSION['userLoggedIn'])) {
+    $userLoggedIn = $_SESSION['userLoggedIn'];
+}
+else {
+    header("Location: register.php");
+}
+
+?>
+
+<html>
+<head>
+    <title>Welcome to Muzik</title>
+</head>
+
+<body>
+    Hello
+</body>
+</html>
