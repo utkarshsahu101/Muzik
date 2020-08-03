4e9960138822f22023beefae81eec7365650208e
Utkarsh Sahu <utkarsh.sahu@alphafortress.com>
2020-08-03 21:04:40 +0530

    songs option menu is added

--- search.php
+++ search.php
@@ -69,7 +69,8 @@ else{
                         </div>
 
                         <div class='trackOptions'>
-                            <img class='optionsButton' src='assets/images/icons/more.png'>
+                            <input type='hidden' class='songId' value='" . $albumSong->getId() . "'>
+                            <img class='optionsButton' src='assets/images/icons/more.png' onclick='showOptionsMenu(this)'>
                         </div>
 
                         <div class='trackDuration'>
@@ -127,3 +128,8 @@ else{
     ?>
 
 </div>
+
+<nav class="optionsMenu">
+    <input type="hidden" class="songId">
+    <?php echo Playlist::getPlaylistsDropdown($con, $userLoggedIn->getUsername()); ?>
+</nav>
